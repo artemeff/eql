5958cf6c10a1b997bcfca69716d1abf8a370cbb0
Tristan Sloughter <t@crashfast.com>
2017-05-10 18:15:37 -0700

    add option to store queries in ets table

--- src/eql.erl
+++ src/eql.erl
@@ -1,5 +1,7 @@
 -module(eql).
 -export([ compile/1
+        , compile/2
+        , new_tab/1
         , get_query/2
         ]).
 
@@ -9,6 +11,26 @@ compile(File) ->
         Error        -> Error
     end.
 
+compile(Tab, File) ->
+    case compile(File) of
+        {ok, Queries} ->
+            ets:insert(Tab, Queries),
+            ok;
+        Error ->
+            Error
+    end.
+
+new_tab(Name) ->
+    ets:new(Name, [named_table, set, {read_concurrency, true}]).
+
+get_query(Name, Tid) when is_reference(Tid)
+                        ; is_atom(Tid) ->
+    case ets:lookup(Tid, Name) of
+        [] ->
+            not_found;
+        [{_, Query}] ->
+            Query
+    end;
 get_query(Name, Proplist) ->
     case lists:keyfind(Name, 1, Proplist) of
         {Name, Value} -> Value;
