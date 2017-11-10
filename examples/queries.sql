413128be837d11522dd8ab6e6950593d9e4d6704
Tristan Sloughter <t@crashfast.com>
2017-11-10 13:30:46 -0800

    handle type casts when parsing named params

--- examples/queries.sql
+++ examples/queries.sql
@@ -1,4 +1,3 @@
-
 --
 -- Get all users from database
 -- :get_all_users
@@ -14,3 +13,7 @@ users WHERE id = ?
 -- name: get_all_schema_users
 SELECT *
 FROM :schema.users
+
+--
+-- name: accept_type_casts
+select '[{"a":"foo"},{"b":"bar"},{"c":"baz"}]'::json->2
